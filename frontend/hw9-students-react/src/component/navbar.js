36ce52ac5d0603daf205b11e90d9ab5d07d6b9fb
vladiazouga <vzouga2021@fau.edu>
2023-04-06 16:38:35 -0400

    fixed navbar

--- frontend/hw9-students-react/src/component/navbar.js
+++ frontend/hw9-students-react/src/component/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -18,13 +18,13 @@ function Navigate() {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav>
-                            <Nav.Link> <Link to='/' className='active'> Home </Link> </Nav.Link>
-                            <Nav.Link> <Link to='/addStudent' className='active'> Add a Student </Link> </Nav.Link>
-                            <Nav.Link> <Link to='/updateStudent' className='active'> Update a Student by ID </Link> </Nav.Link>
-                            <Nav.Link> <Link to='/deleteStudent' className='active'> Delete a Student by ID </Link> </Nav.Link>
-                            <Nav.Link> <Link to='/displayStudent' className='active'> Display a Student by ID </Link> </Nav.Link>
-                            <Nav.Link> <Link to='/listStudents' className='active'> List all Students </Link> </Nav.Link>
-                            <Nav.Link> <Link to='/search' className='active'> Search by Student's Last Name </Link> </Nav.Link>
+                            <NavLink to='/' activeClassName='active'> Home </NavLink>
+                            <NavLink to='/addStudent' activeClassName='active'> Add a Student </NavLink>
+                            <NavLink to='/updateStudent' activeClassName='active'> Update a Student by ID </NavLink>
+                            <NavLink to='/deleteStudent' activeClassName='active'> Delete a Student by ID </NavLink>
+                            <NavLink to='/displayStudent' activeClassName='active'> Display a Student by ID </NavLink>
+                            <NavLink to='/listStudents' activeClassName='active'> List all Students </NavLink>
+                            <NavLink to='/search' activeClassName='active'> Search by Student's Last Name </NavLink>
 
                             
 
